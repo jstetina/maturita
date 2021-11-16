7c0ffe571484d6cbd990efc08aa1eadf3f9b28bc
techguy007 <37512119+techguy007@users.noreply.github.com>
2021-11-16 08:36:21 +0100

    Add files via upload

---
+++ C15.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	printf("zadejte radu cisel: ");
+	
+	int vstup;
+	int soucet = 0;
+	do{
+		scanf("%i",&vstup);
+		if(vstup > 0)
+			soucet += vstup;
+	}while(vstup != 0);
+	
+	printf("soucet je %d",soucet);
+	
+	return 0;
+}
+
