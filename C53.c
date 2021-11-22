1987a79f9a7bacb077d6ee35870595a6d1e9a7c4
techguy007 <jakub.stetina2003@gmail.com>
2021-11-22 08:19:25 +0100

    backup

---
+++ C53.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+struct Komplexni_cislo{
+    int realna_slozka;
+    int imaginarni_slozka;
+};
+
+struct Komplexni_cislo SOUCET_CISEL(struct Komplexni_cislo kc1, struct Komplexni_cislo kc2){
+    struct Komplexni_cislo soucet;
+
+    soucet.realna_slozka = kc1.realna_slozka + kc2.realna_slozka;
+    soucet.imaginarni_slozka = kc1.imaginarni_slozka + kc2.imaginarni_slozka;
+
+    
+    return soucet;
+
+}
+
+
+int main(){
+    struct Komplexni_cislo kc1;
+    struct Komplexni_cislo kc2;
+    struct Komplexni_cislo soucet;
+
+    printf("Soucet komplexnich cisel \n");
+
+    printf("Zadejte prvni cislo ve tvaru a + bi \n");
+    printf("a: ");
+    scanf("%i",&kc1.realna_slozka);
+    printf("b: ");
+    scanf("%i",&kc1.imaginarni_slozka);
+
+    printf("\n");
+
+    printf("Zadejte druhe cislo ve tvaru a + bi \n");
+    printf("a: ");
+    scanf("%i",&kc2.realna_slozka);
+    printf("b: ");
+    scanf("%i",&kc2.imaginarni_slozka);
+
+    printf("cislo 1: %i + %i*i \n",kc1.realna_slozka,kc1.imaginarni_slozka);
+    printf("cislo 2: %i + %i*i \n",kc2.realna_slozka,kc2.imaginarni_slozka);
+
+    soucet = SOUCET_CISEL(kc1,kc2);
+
+    printf("Soucet cisel: %i + %i*i \n",soucet.realna_slozka,soucet.imaginarni_slozka);
+
+    return 0;
+}
--- test.c
+++ test.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void printSizeOf(int intArray[]);
-void printLength(int intArray[]);
-
-int main(int argc, char* argv[])
-{
-    int array[] = { 0, 1, 2, 3, 4, 5, 6 };
-    int cisla[3];
-
-   printf("Length of array: %d\n", (int)( sizeof(cisla) / sizeof(cisla[0]) ));
-}
-
-void printSizeOf(int intArray[])
-{
-    printf("sizeof of parameter: %d\n", (int) sizeof(intArray));
-}
-
-void printLength(int intArray[])
-{
-    printf("Length of parameter: %d\n", (int)( sizeof(intArray) / sizeof(intArray[0]) ));
+int main(){
+    printf("hello \n");
+    delay(100);
+    printf("hi");
+    return 0;
 }
