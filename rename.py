1bdadb32434f22fbbc9a0e331169732d62bfe8c2
techguy007 <jakub.stetina2003@gmail.com>
2022-03-13 16:24:33 +0100

    renamed files for ordering

--- rename.py
+++ rename.py
@@ -2,7 +2,8 @@ import os
 import re
 
 for file in os.listdir():
-    if re.findall("C\d{1,2}.c",file):
-        #print(file)
-        newFile = "C0" + file[1:]
-        os.replace(file,newFile)
+    if re.findall("C\d{2}.c",file):
+        print(file,end=" -> ")
+        new_file = "C0" + file[1:]
+        print(new_file)
+        os.rename(file,new_file)
